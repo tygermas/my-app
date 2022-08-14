d8b348cf1c43bfd0736085e6c84fb23d863b6511
Gyana Mishra <gyanaranjan.shilu@gmail.com>
2022-08-14 20:02:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloaddd--gyan.
+          Edit <code>src/App.js</code> and save to reloaddd--gyan mishra.
         </p>
         <a
           className="App-link"
